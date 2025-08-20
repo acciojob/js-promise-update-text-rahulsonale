bbddf050e42cd214479860b10304bba6a1250760
Rahulsonale <rahulsonale002@gmail.com>
2025-08-20 19:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function SayHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello World");
+    }, 1000);
+  });
+}
+
+SayHello().then((message) => {
+  document.querySelector("p").textContent = message;
+});
